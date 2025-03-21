6b757a2c6a860cbb3b05b308be412bb2b0b215e5
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-21 11:16:09 +0100

    Update

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { revalidateTag } from 'next/cache';
 import Form from 'next/form';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import React, { useTransition } from 'react';
+import { reval } from '@/data/revalidate';
 import type { TaskStatus } from '@/types/task';
 import SearchStatus from './ui/SearchStatus';
 
@@ -16,36 +16,35 @@ export default function Search() {
   const [isPending, startTransition] = useTransition();
 
   return (
-    <Form action="" className="relative flex w-full flex-col gap-1 sm:w-fit" key={activeTab}>
-      <label className="font-semibold uppercase" htmlFor="search">
-        Search
-      </label>
-      <input
-        autoComplete="off"
-        id="search"
-        onChange={e => {
-          const newSearchParams = new URLSearchParams(searchParams.toString());
-          newSearchParams.set('q', e.target.value);
-          startTransition(() => {
-            router.push(`?${newSearchParams.toString()}`, {
-              scroll: false,
+    <>
+      <Form action="" className="relative flex w-full flex-col gap-1 sm:w-fit" key={activeTab}>
+        <label className="font-semibold uppercase" htmlFor="search">
+          Search
+        </label>
+        <input
+          autoComplete="off"
+          id="search"
+          onChange={e => {
+            const newSearchParams = new URLSearchParams(searchParams.toString());
+            newSearchParams.set('q', e.target.value);
+            startTransition(() => {
+              router.push(`?${newSearchParams.toString()}`, {
+                scroll: false,
+              });
             });
-          });
-        }}
-        defaultValue={q}
-        className="w-full pl-10 sm:w-96"
-        name="q"
-        placeholder="Search in task title or description..."
-        type="search"
-      />
-      <SearchStatus searching={isPending} />
-      <button
-        onClick={async () => {
-          'use server';
-          revalidateTag('cached-content');
-        }}
-      />
-    </Form>
+          }}
+          defaultValue={q}
+          className="w-full pl-10 sm:w-96"
+          name="q"
+          placeholder="Search in task title or description..."
+          type="search"
+        />
+        <SearchStatus searching={isPending} />
+      </Form>
+      <form action={reval}>
+        <button type="submit">Reval</button>
+      </form>
+    </>
   );
 }
 
