ccce34465dda83b63dccb2401bc91e712b506da6
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-27 22:58:11 +0300

    Add back PPR

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
+    ppr: true,
     reactCompiler: true,
     staleTimes: {
       dynamic: 30,
